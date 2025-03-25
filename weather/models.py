9b42b09574bbbdb31b9285d4881e8bcb91998728
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-03-25 16:42:37 +0100

    adapt sql models to pypsdm version

--- weather/models.py
+++ weather/models.py
@@ -1,69 +1,24 @@
 from datetime import datetime
+from typing import Any, ClassVar, Dict
 
-from geoalchemy2 import Geography
-from geoalchemy2.elements import WKBElement
-from pydantic import ConfigDict
-from shapely import Geometry
-from shapely.geometry.point import Point
-from shapely.wkb import loads, dumps
+from geoalchemy2 import Geometry, WKBElement, Geography
+from shapely import Point
+from shapely.wkb import loads
 from sqlalchemy import Column
 from sqlmodel import Field, SQLModel
 
 
-class Coordinate(SQLModel, table=True):
-    """Represents a geographical coordinate."""
+def ensure_bytes(wkb_element):
+    # Get the data from the WKBElement
+    data = wkb_element.data
+    # Check if it's a memoryview and convert it to bytes if necessary
+    if isinstance(data, memoryview):
+        return data.tobytes()
+    elif isinstance(data, bytes):
+        return data
+    else:
+        raise TypeError("Unexpected data type for WKBElement data")
 
-    # Allow arbitrary types in model configuration
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-
-    id: int = Field(default=None, primary_key=True)
-
-    # Use WKBElement type with the Geography column
-    coordinate: WKBElement = Field(
-        sa_column=Column(
-            Geography(geometry_type="POINT", srid=4326, spatial_index=False)
-        )
-    )
-
-    def __init__(self, id: int, coordinate: Geometry):
-        self.id = id
-        self.coordinate = coordinate
-
-    def __eq__(self, other):
-        return self.id == other.id
-
-    def __hash__(self):
-        return hash(self.id)
-
-    @property
-    def point(self) -> Point:
-        if isinstance(self.coordinate, WKBElement):
-            wkb_str = str(self.coordinate)
-            coordinate = bytes.fromhex(wkb_str)
-        else:
-            coordinate = self.coordinate
-        return loads(coordinate)
-
-    @property
-    def latitude(self) -> float:
-        return self.point.y
-
-    @property
-    def y(self) -> float:
-        return self.point.y
-
-    @property
-    def longitude(self) -> float:
-        return self.point.x
-
-    @property
-    def x(self) -> float:
-        return self.point.x
-
-    @staticmethod
-    def from_xy(id: int, x: float, y: float) -> "Coordinate":
-        point = Point(x, y)
-        return Coordinate(id=id, coordinate=point)
 
 class WeatherValue(SQLModel, table=True):
     """
@@ -110,3 +65,57 @@ class WeatherValue(SQLModel, table=True):
             "u131m": "wind_velocity_u",
             "v131m": "wind_velocity_v",
         }
+
+
+class Coordinate(SQLModel, table=True):
+    """Represents a geographical coordinate."""
+
+    model_config: ClassVar[Dict[str, Any]] = {"arbitrary_types_allowed": True}
+
+    id: int = Field(default=None, primary_key=True)
+
+    coordinate: Geography = Field(
+        sa_column=Column(
+            Geography(geometry_type="POINT", srid=4326, spatial_index=False)
+        )
+    )
+
+    def __init__(self, id: int, coordinate: Geography):
+        self.id = id
+        self.coordinate = coordinate
+
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __hash__(self):
+        return hash(self.id)
+
+
+
+
+    @property
+    def point(self) -> Point:
+        return loads(ensure_bytes(self.coordinate))
+
+    @property
+    def latitude(self) -> float:
+        return self.point.y
+
+    @property
+    def y(self) -> float:
+        return self.point.y
+
+    @property
+    def longitude(self) -> float:
+        return self.point.x
+
+    @property
+    def x(self) -> float:
+        return self.point.x
+
+    @staticmethod
+    def from_xy(id: int, x: float, y: float) -> "Coordinate":
+        point = Point(x, y)
+        wkb = WKBElement(point.wkb, srid=4326)
+        return Coordinate(id, wkb)
+
