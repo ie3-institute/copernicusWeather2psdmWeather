ce6cba0c11ab628d4f2ff4d0e45de7b37c32e10b
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-07-11 13:07:29 +0200

    raising error on not found config file

--- weather/config.py
+++ weather/config.py
@@ -19,13 +19,7 @@ def load_config(config_path="config.yaml"):
         dict: Configuration with resolved paths
     """
     if not os.path.exists(config_path):
-        print(f"Configuration file {config_path} not found. Using default values.")
-        return {
-            "ROOT_DIR": str(Path.cwd()),
-            "input_dir": str(Path.cwd() / "input"),
-            "file_name_base": "westfalia_2024_06_01-2024_06_07",
-            "batch_size": 1000,
-        }
+        raise FileNotFoundError(f"Configuration file {config_path} not found.")
 
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
